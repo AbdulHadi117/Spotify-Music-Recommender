b30864f32dfd509d7255f2f14bbee09224c824d3
Abdul Hadi <117abdulhadi@gmail.com>
2025-02-22 06:25:24 +0500

    Load Environment Files

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from flask import Flask, redirect, request, session, url_for, render_template, j
 import spotipy, os,  logging
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
+
+app = Flask(__name__)
