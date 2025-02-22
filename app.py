2b27fa9b6b104675972d355bf01c35a8c32fae9a
Abdul Hadi <117abdulhadi@gmail.com>
2025-02-22 06:26:41 +0500

    Initialize Spotify Client

--- app.py
+++ app.py
@@ -7,8 +7,25 @@ from dotenv import load_dotenv
 # Load environment variables from a .env file
 load_dotenv()
 
+# Retrieve Spotify API credentials from environment variables
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
 app = Flask(__name__)
 
 # Set the secret key for session management
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 app.config["SESSION_COOKIE_NAME"] = "Spotify Music Recommender"
+
+# Initialize Spotify OAuth client
+sp_oauth = SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope=(
+        "user-read-email user-read-private user-library-modify "
+        "user-library-read user-read-recently-played user-top-read "
+        "playlist-read-private playlist-modify-private playlist-modify-public"
+    )
+)
